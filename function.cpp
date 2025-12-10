6805744f3e2bc2eef58be2212ecf939d3efad0cf
kennysirai <kennysirai@student.usm.my>
2025-12-10 12:47:55 +0800

    Progress

---
+++ function.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+#include <iostream>
+
+void welcome() // Coded by KONG WEI HANG
+{
+    cout << "______________________________________________________" << endl;
+    cout << " __    __    ___  _        __   ___   ___ ___    ___  " << endl;
+    cout << "|  |__|  |  /  _]| |      /  ] /   \\ |   |   |  /  _] " << endl;
+    cout << "|  |  |  | /  [_ | |     /  / |     || _   _ | /  [_  " << endl;
+    cout << "|  |  |  ||    _]| |___ /  /  |  O  ||  \\_/  ||    _] " << endl;
+    cout << "|  `  '  ||   [_ |     /   \\_ |     ||   |   ||   [_  " << endl;
+    cout << " \\      / |     ||     \\     ||     ||   |   ||     | " << endl;
+    cout << "  \\_/\\_/  |_____||_____|\\____| \\___/ |___|___||_____| " << endl;
+    cout << "______________________________________________________" << endl;     
+    cout << "Welcome dear valued customers to our shopping list system site!"         << endl;
+    cout << "We highly value the performance of our system in bringing our users: "                << endl;
+    cout << "\t\t\t     THE BEST!!!"                                                                                  << endl;
+    cout << "\t\t\t     THE SAFEST!!!"                                                                                 << endl;
+    cout << "\t\t\t     THE SMOOTHEST!!!"                                                                               << endl;
+    cout << "_______________________GROUP 44 WELCOME U_______________________" << endl;
+    cout << "" << endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+#include <ctime>
+#include <thread>
+using namespace std;
+#include "function.cpp"
+int main()
+{
+
+welcome();
+this_thread::sleep_for(chrono::milliseconds(30000));
+cout << "\033[2J\033[1;1H";
+
+    
+    return 0;
+}
